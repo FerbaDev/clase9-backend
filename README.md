5bac9ab0289cac11eebc8ff11201e1b6b4f53893
FerbaDev <elferbarron1@gmail.com>
2024-03-21 01:47:38 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Mongo Avanzado I y II
+
+instalacion ----------- 00:09
+
+que es la indexacion? ------- 00:13
+
+conexión a compass ---------- 00:16
+
+se crean models ------------- 00:28
+
+dejé en 00:31
---
+++ src/models/user.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    nombre: {
+        type: String
+    },
+    apellido: String,
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    edad: {
+        type: Number
+    }
+})
+
+const userModel = mongoose.model("usuarios", userSchema);
+
+export default userModel;
