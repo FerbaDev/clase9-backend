c73c9d73a899b4d613274932ee148150a50fe9d1
FerbaDev <elferbarron1@gmail.com>
2024-03-23 02:14:08 -0300

    error en readme

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import "dotenv/config";
 //     .then(() => console.log("Conectado a Mongo DB"))
 //     .catch((error) => console.log("Conexión a Mongo DB falló", error)); 
 
-//     const respuesta = await UserModel.find({nombre: "Fernando"}).explain("executioStats");
+//     const respuesta = await UserModel.find({nombre: "Fernando"}).explain("executionStats");
 //     //recordemos que find devuelve un array de objetos por lo tento podemos aplicar el metodo explain para ver las estaditicas de la consulta, exlpain puede llevar elparametro executionStats para obtener detalles d elos tiempos de demora de la consulta.
 //     console.log(respuesta);
 // }
